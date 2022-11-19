97e0aa336be52de808aed999c965a6357765e61d
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2022-11-19 13:56:02 -0300

    hiperLink

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -4,14 +4,29 @@ export default function Topbar() {
   return (
     <nav className="navbar">
       <ul className="navMenu">
-        <li>home</li>
-        <li>serviços</li>
-        <li>mental coach</li>
-        <li>depoimentos</li>
-        <li>Contato</li>
-        <button className='botaoTop'>
+        <li>
+          <a href="/">home</a>
+        </li>
+        <li>
+          <a href="#servicos">serviços</a>
+        </li>
+        <li>
+          <a href="#mentalCoach">mental coach</a>
+        </li>
+        <li>
+          <a href="#depoimentos">depoimentos</a>
+        </li>
+        <li>
+          <a href="#contato">Contato</a>
+        </li>
+        <button className="botaoTop">
           <img src="../image/whatsapp.svg" alt="WhatsApp" />
-          WhatsApp
+          <a
+            href="https://api.whatsapp.com/send?phone=5521972680407&text=Quero%20ter%20mentalidade%20de%20campe%C3%A3o"
+            target="_blank"
+          >
+            WhatsApp
+          </a>
         </button>
       </ul>
     </nav>
